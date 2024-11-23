ff3e6eafbb7ff8191fd60e3b7d9ff4ee439fb1bd
NguyenHuy2k5 <Huy.BKC15245@student.bkacad.edu.vn>
2024-11-24 01:56:31 +0700

    Update tesst.java

--- Test/tesst.java
+++ Test/tesst.java
@@ -2,5 +2,7 @@ public class tesst {
     public static void main(String[] args) {
         System.out.println("tesst");
         System.out.println("cap nhat sao 1h");
+        System.out.println("hvh2k5");
+        
     }
 }
